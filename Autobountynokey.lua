4e2e11915fd544ce8897122191f91fa813d67d9b
MEOW HUB🐾 <ngo416203@gmail.com>
2025-12-26 20:33:22 +0700

    Create Autobountynokey.lua

---
+++ Autobountynokey.lua
@@ -0,0 +1,14 @@
+--getgenv().team = "Marines" -- Change to "Pirates" if preferred
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer:FindFirstChild("DataLoaded")
+
+-- Reliable team selection method
+if game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("Main (minimal)") then
+    repeat
+        wait()
+        local l_Remotes_0 = game.ReplicatedStorage:WaitForChild("Remotes")
+        l_Remotes_0.CommF_:InvokeServer("SetTeam", getgenv().team)
+        task.wait(1)
+    until not game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("Main (minimal)")
+end
+--------
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MEOW-HUB-DEV/SCRIPT-FREE/refs/heads/main/Autobounty.lua"))()
